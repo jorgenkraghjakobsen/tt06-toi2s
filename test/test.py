bea023978fcd4682517d04b7fae31854a2def6ed
Jorgen Kragh Jakobsen <jkj@myrun.dk>
2024-03-20 15:21:40 +0100

    Fixed dummy cocotb test

--- test/test.py
+++ test/test.py
@@ -24,9 +24,9 @@ async def test_adder(dut):
 
   # Set the input values, wait one clock cycle, and check the output
   dut._log.info("Test")
-  dut.ui_in.value = 20
+  dut.ui_in.value = 30
   dut.uio_in.value = 30
 
   await ClockCycles(dut.clk, 1)
 
-  assert dut.ui_in.value == 20
+  assert dut.ui_in.value == 30
